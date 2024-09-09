768fb34996f62be14d32f60fb219092e753b9838
Đỗ Hữu Đại <daidh152001@gmail.com>
2024-09-09 18:39:38 +0700

    Update __init__.py

--- api/controllers/search_api/__init__.py
+++ api/controllers/search_api/__init__.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 
 from libs.external_api import ExternalApi
 
-bp = Blueprint('search_api', __name__, url_prefix='/search/api')
+bp = Blueprint('search_api', __name__, url_prefix='/search')
 api = ExternalApi(bp)
 
 
