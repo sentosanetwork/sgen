33cbdfcd30a4191e8a0750eb0faeec2097714514
John Doe <johndoe@example.com>
2024-10-16 16:09:10 +0700

    ALLOW_FILE_EXTENSIONS

--- web/types/app.ts
+++ web/types/app.ts
@@ -385,7 +385,7 @@ export enum TtsAutoPlay {
   disabled = 'disabled',
 }
 
-export const ALLOW_FILE_EXTENSIONS = ['png', 'jpg', 'jpeg', 'webp', 'gif']
+export const ALLOW_FILE_EXTENSIONS = ['png', 'jpg', 'jpeg', 'webp', 'gif', 'pdf']
 
 export type VisionSettings = {
   enabled: boolean
