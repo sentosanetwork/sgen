29628ecf942be7673c022dbc1573a9d3405c746a
John Doe <johndoe@example.com>
2024-10-18 09:52:19 +0700

    api

--- api/app.py
+++ api/app.py
@@ -213,23 +213,23 @@ def register_blueprints(app):
         service_api_bp,
         allow_headers=["Content-Type", "Authorization", "X-App-Code"],
         methods=["GET", "PUT", "POST", "DELETE", "OPTIONS", "PATCH"],
-        origins="*",  # Allow all origins
     )
     app.register_blueprint(service_api_bp)
 
     CORS(
         web_bp,
-        resources={r"/*": {"origins": "*"}},  # Allow all origins
+        resources={r"/*": {"origins": app.config["WEB_API_CORS_ALLOW_ORIGINS"]}},
         supports_credentials=True,
         allow_headers=["Content-Type", "Authorization", "X-App-Code"],
         methods=["GET", "PUT", "POST", "DELETE", "OPTIONS", "PATCH"],
         expose_headers=["X-Version", "X-Env"],
     )
+
     app.register_blueprint(web_bp)
 
     CORS(
         console_app_bp,
-        resources={r"/*": {"origins": "*"}},  # Allow all origins
+        resources={r"/*": {"origins": app.config["CONSOLE_CORS_ALLOW_ORIGINS"]}},
         supports_credentials=True,
         allow_headers=["Content-Type", "Authorization"],
         methods=["GET", "PUT", "POST", "DELETE", "OPTIONS", "PATCH"],
@@ -238,7 +238,7 @@ def register_blueprints(app):
 
     app.register_blueprint(console_app_bp)
 
-    CORS(files_bp, allow_headers=["Content-Type"], methods=["GET", "PUT", "POST", "DELETE", "OPTIONS", "PATCH"], origins="*")  # Allow all origins
+    CORS(files_bp, allow_headers=["Content-Type"], methods=["GET", "PUT", "POST", "DELETE", "OPTIONS", "PATCH"])
     app.register_blueprint(files_bp)
 
     app.register_blueprint(inner_api_bp)
@@ -247,15 +247,14 @@ def register_blueprints(app):
         stock_api_bp,
         allow_headers=["Content-Type", "Authorization", "X-App-Code"],
         methods=["GET", "PUT", "POST", "DELETE", "OPTIONS", "PATCH"],
-        origins="*",  # Allow all origins
     )
     app.register_blueprint(stock_api_bp)
 
     CORS(search_api_bp,
          allow_headers=["Content-Type", "Authorization", "X-App-Code"],
          methods=["GET", "PUT", "POST", "DELETE", "OPTIONS", "PATCH"],
-         origins="*"  # Allow all origins
     )
+
     app.register_blueprint(search_api_bp)
 
 # create app
