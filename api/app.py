e8fdef2dc6299138adb32041681b44a017ecde73
Tuan <tuantd22@xanhsm.com>
2024-10-22 03:43:18 +0700

    CORS

--- api/app.py
+++ api/app.py
@@ -18,7 +18,7 @@ import warnings
 from logging.handlers import RotatingFileHandler
 
 from flask import Flask, Response, request
-from flask_cors import CORS
+# from flask_cors import CORS
 from werkzeug.exceptions import Unauthorized
 
 import contexts
@@ -209,51 +209,51 @@ def register_blueprints(app):
     from controllers.stock_api import bp as stock_api_bp
     from controllers.search_api import bp as search_api_bp
 
-    CORS(
-        service_api_bp,
-        allow_headers=["Content-Type", "Authorization", "X-App-Code"],
-        methods=["GET", "PUT", "POST", "DELETE", "OPTIONS", "PATCH"],
-    )
+    # CORS(
+    #     service_api_bp,
+    #     allow_headers=["Content-Type", "Authorization", "X-App-Code"],
+    #     methods=["GET", "PUT", "POST", "DELETE", "OPTIONS", "PATCH"],
+    # )
     app.register_blueprint(service_api_bp)
 
-    CORS(
-        web_bp,
-        resources={r"/*": {"origins": app.config["WEB_API_CORS_ALLOW_ORIGINS"]}},
-        supports_credentials=True,
-        allow_headers=["Content-Type", "Authorization", "X-App-Code"],
-        methods=["GET", "PUT", "POST", "DELETE", "OPTIONS", "PATCH"],
-        expose_headers=["X-Version", "X-Env"],
-    )
+    # CORS(
+    #     web_bp,
+    #     resources={r"/*": {"origins": app.config["WEB_API_CORS_ALLOW_ORIGINS"]}},
+    #     supports_credentials=True,
+    #     allow_headers=["Content-Type", "Authorization", "X-App-Code"],
+    #     methods=["GET", "PUT", "POST", "DELETE", "OPTIONS", "PATCH"],
+    #     expose_headers=["X-Version", "X-Env"],
+    # )
 
     app.register_blueprint(web_bp)
 
-    CORS(
-        console_app_bp,
-        resources={r"/*": {"origins": app.config["CONSOLE_CORS_ALLOW_ORIGINS"]}},
-        supports_credentials=True,
-        allow_headers=["Content-Type", "Authorization"],
-        methods=["GET", "PUT", "POST", "DELETE", "OPTIONS", "PATCH"],
-        expose_headers=["X-Version", "X-Env"],
-    )
+    # CORS(
+    #     console_app_bp,
+    #     resources={r"/*": {"origins": app.config["CONSOLE_CORS_ALLOW_ORIGINS"]}},
+    #     supports_credentials=True,
+    #     allow_headers=["Content-Type", "Authorization"],
+    #     methods=["GET", "PUT", "POST", "DELETE", "OPTIONS", "PATCH"],
+    #     expose_headers=["X-Version", "X-Env"],
+    # )
 
     app.register_blueprint(console_app_bp)
 
-    CORS(files_bp, allow_headers=["Content-Type"], methods=["GET", "PUT", "POST", "DELETE", "OPTIONS", "PATCH"])
+    # CORS(files_bp, allow_headers=["Content-Type"], methods=["GET", "PUT", "POST", "DELETE", "OPTIONS", "PATCH"])
     app.register_blueprint(files_bp)
 
     app.register_blueprint(inner_api_bp)
 
-    CORS(
-        stock_api_bp,
-        allow_headers=["Content-Type", "Authorization", "X-App-Code"],
-        methods=["GET", "PUT", "POST", "DELETE", "OPTIONS", "PATCH"],
-    )
+    # CORS(
+    #     stock_api_bp,
+    #     allow_headers=["Content-Type", "Authorization", "X-App-Code"],
+    #     methods=["GET", "PUT", "POST", "DELETE", "OPTIONS", "PATCH"],
+    # )
     app.register_blueprint(stock_api_bp)
 
-    CORS(search_api_bp,
-         allow_headers=["Content-Type", "Authorization", "X-App-Code"],
-         methods=["GET", "PUT", "POST", "DELETE", "OPTIONS", "PATCH"],
-    )
+    # CORS(search_api_bp,
+    #      allow_headers=["Content-Type", "Authorization", "X-App-Code"],
+    #      methods=["GET", "PUT", "POST", "DELETE", "OPTIONS", "PATCH"],
+    # )
 
     app.register_blueprint(search_api_bp)
 
